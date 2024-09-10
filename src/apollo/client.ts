080b85940dd6eb7666c3cd0236a72e13ecd29ba6
Ruentgen <ruentgen@aienergyshop.com.au>
2024-09-10 14:27:08 +0800

    remove apollo client

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,14 +1,14 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+// import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
-const PROTOCOL = process.env.BASE_PROTOCOL;
-const HOST = process.env.BASE_URL_HOST;
+// const PROTOCOL = process.env.BASE_PROTOCOL;
+// const HOST = process.env.BASE_URL_HOST;
 
-const httpLink = new HttpLink({
-  uri: `${PROTOCOL}://${HOST}/graphql`,
-  credentials: "include",
-});
+// const httpLink = new HttpLink({
+//   uri: `${PROTOCOL}://${HOST}/graphql`,
+//   credentials: "include",
+// });
 
-export const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache(),
-});
+// export const client = new ApolloClient({
+//   link: httpLink,
+//   cache: new InMemoryCache(),
+// });
