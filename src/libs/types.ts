4c332e81f0c62d7e3010e739905dfcf0ff8e1800
Ruentgen <ruentgen@aienergyshop.com.au>
2024-09-16 13:34:41 +0800

    commneted posible return from about section query

--- src/libs/types.ts
+++ src/libs/types.ts
@@ -8,3 +8,25 @@ export type AccordionData = {
   title: string;
   content: string[];
 };
+
+// ABOUT SECTION QUERY TYPES
+// export type AboutSectionQueryType = {
+//   aboutSection: {
+//     data: {
+//       attributes: {
+//         heading: string;
+//         sub_heading: string;
+//         paragraph: string;
+//         button_title: string;
+//         baground_image: {
+//           data: {
+//             attributes: {
+//               url: string;
+//               alternativeText: string;
+//             };
+//           };
+//         };
+//       };
+//     };
+//   };
+// };
