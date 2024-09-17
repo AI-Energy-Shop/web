b27dc493742c56bc1cc00a8858c8dca0e23a6cad
Ruentgen <ruentgen@aienergyshop.com.au>
2024-09-17 09:48:11 +0800

    add error handler

--- src/components/Sections/BannerSection.tsx
+++ src/components/Sections/BannerSection.tsx
@@ -7,23 +7,16 @@ import { useQuery } from "@apollo/client";
 
 const BannerSection = () => {
   const { data, loading } = useQuery(BANNER_OPERATION.Queries.getBannerImages);
-
-  if (loading) {
-    return (
-      <div className="max-w-[1200px] h-[44.4vh] lg:max-h-[33.3vh] m-auto">
-        Loading...
-      </div>
-    );
-  }
-
-  const {
-    bannerImages: { data: images },
-  } = data;
-
   return (
     <section className="banner-section w-full h-auto md:h-[40.4vh]  lg:max-h-[33.3vh]">
       <div className="inner-container max-w-[1200px] h-full m-auto">
-        <Carousel.ImageCarousel data={images} />
+        {!loading && (
+          <Carousel.ImageCarousel
+            data={data?.bannerImages?.data}
+            loading={loading}
+          />
+        )}
+
         <Spacer classStyle="h-[5px] lg:h-[10px] gradient-effect" />
       </div>
     </section>
