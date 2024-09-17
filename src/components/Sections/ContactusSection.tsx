9afa94c0681c458ab577fa9add04778cf44a0cbe
Ruentgen <ruentgen@aienergyshop.com.au>
2024-09-17 09:49:12 +0800

    integrate strapi data

--- src/components/Sections/ContactusSection.tsx
+++ src/components/Sections/ContactusSection.tsx
@@ -2,15 +2,21 @@
 import { firaSansFont, muktaVaani } from "@/assets/fonts/fonts";
 import React from "react";
 import Image from "next/image";
-
+import { useQuery } from "@apollo/client";
+import CONTACT_SECTION_OPERATION from "@/graphql/contact-section";
 const ContactusSection = () => {
+  const { data, loading } = useQuery(
+    CONTACT_SECTION_OPERATION.Queries.getContactSection
+  );
+
   return (
     <section className="about-section w-full h-auto">
       <div
         className="
-        h-[200px] w-full
-        md:h-[300px] 
-        lg:max-w-[1200px] lg:h-[320px] lg:m-auto"
+          h-[200px] w-full
+          md:h-[300px] 
+          lg:max-w-[1200px] lg:h-[320px] lg:m-auto
+        "
       >
         <div
           className="
@@ -23,14 +29,17 @@ const ContactusSection = () => {
               lg:w-[65%] lg:clip-path-right-70
             "
           >
-            <Image
-              alt=""
-              fill
-              sizes="100vh"
-              quality={100}
-              className="object-cover"
-              src="/images/background/AES-Customer-Chat-w-Solplanet.jpg"
-            />
+            {!loading && (
+              <Image
+                fill
+                alt=""
+                priority
+                sizes="100vh"
+                quality={100}
+                className="object-cover"
+                src={`${process.env.BASE_PROTOCOL}://${process.env.BASE_URL_HOST}${data.contactusSection.data.attributes.image.data.attributes.url}`}
+              />
+            )}
           </div>
           <div
             className="absolute top-0 right-0 -z-1
@@ -45,7 +54,7 @@ const ContactusSection = () => {
                 lg:text-[28px]
               "
             >
-              Service Across Australia
+              {!loading && data.contactusSection.data.attributes.heading}
             </h2>
             <p
               style={muktaVaani.style}
@@ -55,19 +64,19 @@ const ContactusSection = () => {
                 lg:max-w-[30%] lg:text-end
               "
             >
-              Ai Energy Shop is always here to answer your questions, Serving
-              costumers nation wide, we&apos;re always here to provide the
-              support you need.
+              {!loading && data.contactusSection.data.attributes.paragraph}
             </p>
-            <button
-              style={firaSansFont.style}
-              className="
-                bg-yellow-aes-yellow max-w-[120px] min-w-[120px] uppercase leading-5 text-lg p-1
-                md:max-w-[150px] md:min-w-[140px]
-              "
-            >
-              contact us
-            </button>
+            {!loading && (
+              <button
+                style={firaSansFont.style}
+                className="
+                    bg-yellow-aes-yellow max-w-[120px] min-w-[120px] uppercase leading-5 text-lg p-1
+                    md:max-w-[150px] md:min-w-[140px]
+                  "
+              >
+                {data.contactusSection.data.attributes.button_title}
+              </button>
+            )}
           </div>
         </div>
       </div>
