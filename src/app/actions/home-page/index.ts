daf9ebc3d85c866c48a34dd0013e8019e5061e1f
Ruentgen <ruentgen@aienergyshop.com.au>
2024-09-20 13:36:28 +0800

    serverside data fetching

---
+++ src/app/actions/home-page/index.ts
@@ -0,0 +1,17 @@
+"use server";
+
+import { client } from "@/apollo/client";
+import HOMEPAGE_OPERATIONS from "@/graphql/home-page";
+import { HomePageRes } from "@/libs/types";
+
+export const homePage = async (): Promise<HomePageRes> => {
+  try {
+    const res = await client.query<HomePageRes>({
+      query: HOMEPAGE_OPERATIONS.Queries.homePage,
+    });
+
+    return res.data;
+  } catch (error: any) {
+    return error.message;
+  }
+};
