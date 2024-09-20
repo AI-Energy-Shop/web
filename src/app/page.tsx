05a811ac9dca8c2b72a4f89b0d460931343cffe2
Ruentgen <ruentgen@aienergyshop.com.au>
2024-09-20 13:36:09 +0800

    update serverside fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
 import Components from "@/components";
+import { homePage } from "./actions/home-page";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const res = await homePage();
   return (
     <main className="w-full min-h-screen bg-yellow-light-yellow-50 ">
-      <Components.Sections.BannerSection />
+      <Components.Sections.BannerSection
+        data={res.homePage.data.attributes.banner_images}
+      />
 
-      <Components.Sections.AboutusSection />
+      <Components.Sections.AboutusSection
+        data={res.homePage.data.attributes.about_section}
+      />
 
-      <Components.Sections.ContactusSection />
+      <Components.Sections.ContactusSection
+        data={res.homePage.data.attributes.contactus_section}
+      />
 
       <Components.Sections.NewsletterSection />
     </main>
