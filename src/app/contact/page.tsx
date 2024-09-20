cacd578357090b44b19ce44fdd5b089b47dfa5d4
Ruentgen <ruentgen@aienergyshop.com.au>
2024-09-20 13:36:43 +0800

    update serverside data fetching

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,28 @@
 import Components from "@/components";
 import React from "react";
+import { getContactPage } from "../actions/contact-page";
+const ContactPage = async () => {
+  const contact = await getContactPage();
 
-const ContactPage = () => {
   return (
-    <main className="min-h-[100vh] w-full">
-      <Components.Sections.ContactFormSection />
+    <main className="min-h-[100vh] w-full h-auto p-5 bg-yellow-light-yellow">
+      <div className="inner-container w-full lg:max-w-[1200px] h-full mx-auto">
+        <h1 className="page-title text-lg w-full text-center font-bold uppercase my-5">
+          Contact Us
+        </h1>
+
+        <Components.Sections.ContactDetails
+          data={contact.contactPage.data.attributes.contact_details_section}
+        />
+
+        <Components.Sections.WarehouseSection
+          data={contact.contactPage.data.attributes.warehouse_location}
+        />
+
+        <Components.Sections.InquerySection />
+
+        <Components.Sections.NewsletterSection />
+      </div>
     </main>
   );
 };
