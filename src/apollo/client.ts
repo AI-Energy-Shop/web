98f85f83432ab781efe389d9bf32c01b2ae76391
Ruentgen <ruentgen@aienergyshop.com.au>
2024-09-20 13:35:50 +0800

    testing ssr

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,14 +1,31 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  // HttpLink,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
 
 const PROTOCOL = process.env.BASE_PROTOCOL;
 const HOST = process.env.BASE_URL_HOST;
 
-const httpLink = new HttpLink({
-  uri: `${PROTOCOL}://${HOST}/graphql`,
-  credentials: "include",
-});
+// const httpLink = new HttpLink({
+//   uri: `${PROTOCOL}://${HOST}/graphql`,
+//   credentials: "include",
+// });
+
+// export const client = new ApolloClient({
+//   link: httpLink,
+//   cache: new InMemoryCache(),
+// });
 
 export const client = new ApolloClient({
-  link: httpLink,
+  ssrMode: true,
+  link: createHttpLink({
+    uri: `${PROTOCOL}://${HOST}/graphql`,
+    // credentials: 'same-origin',
+    // headers: {
+    //   cookie: req.header('Cookie'),
+    // },
+  }),
   cache: new InMemoryCache(),
 });
