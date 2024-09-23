eeafdc73edb85299f4728ffff7f1924403a5d899
Ruentgen <ruentgen@aienergyshop.com.au>
2024-09-23 11:06:00 +0800

    fixing deployment error

--- next.config.mjs
+++ next.config.mjs
@@ -28,14 +28,6 @@ const nextConfig = {
     ],
   },
   reactStrictMode: false,
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": path.resolve(__dirname, "./"),
-    };
-
-    return config;
-  },
 };
 
 export default nextConfig;
