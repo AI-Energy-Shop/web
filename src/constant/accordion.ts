44779f955c4e1b5af404bb6c17a8195ad5a74de4
UBDevRuent <ruentgencomia@umbradigitalcompany.com>
2024-11-04 21:54:25 +0800

    added constant dir & update constant structure

---
+++ src/constant/accordion.ts
@@ -0,0 +1,12 @@
+export const FOOTER_ACCORDION_DATA = [
+  {
+    id: 1,
+    title: 'Our Company',
+    content: ['About Us', 'Contact Us'],
+  },
+  {
+    id: 2,
+    title: 'Resources',
+    content: ['Downloads', 'Shift Trade', 'STC Trading'],
+  },
+];
