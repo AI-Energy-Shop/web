fcce106691de6a8a0a676ffee176ec593a69921a
UBDevRuent <ruentgencomia@umbradigitalcompany.com>
2024-12-12 08:18:10 +0800

    fixed build error

--- src/app/(home)/[slug]/page.tsx
+++ src/app/(home)/[slug]/page.tsx
@@ -1,4 +1,4 @@
-export const dynamic = 'force-dynamic'
+export const dynamic = 'force-dynamic';
 import { getPage } from '@/app/actions';
 import Components from '@/components';
 import React from 'react';
