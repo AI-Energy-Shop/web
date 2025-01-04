e91664cb4427ffbb72097b90b638da975df5d586
UBDevRuent <ruentgencomia@umbradigitalcompany.com>
2025-01-04 11:56:30 +0800

    remove Fragment container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,7 @@ export default async function HomePage() {
   const data = await getPage('/');
   return (
     <main className="w-full min-h-screen">
-      <>
-        <Components.DynamicComponentRenderer data={data} />
-      </>
+      <Components.DynamicSections data={data} />
     </main>
   );
 }
