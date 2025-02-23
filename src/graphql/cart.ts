22ed49537327cf9cb6ff9f9db49cc807b6a29357
javdevruent <ruentgen@aienergy.com.au>
2025-02-23 11:17:02 +0800

    update gql query

--- src/graphql/cart.ts
+++ src/graphql/cart.ts
@@ -3,10 +3,11 @@ import { graphql } from '@/lib/gql';
 const schema = {
   Query: {
     carts: graphql(`
-      query Carts($filters: CartFiltersInput) {
-        carts(filters: $filters) {
+      query Carts($filters: CartFiltersInput, $pagination: PaginationArg) {
+        carts(filters: $filters, pagination: $pagination) {
           documentId
           item {
+            id
             title
             quantity
             price
@@ -14,18 +15,20 @@ const schema = {
             model
             image
           }
-          updatedAt
           createdAt
+          updatedAt
+          publishedAt
         }
       }
     `),
   },
   Mutation: {
     addToCart: graphql(`
-      mutation CreateCart($data: CartInput!) {
-        createCart(data: $data) {
+      mutation AddToCart($data: CartItemInput!) {
+        addToCart(data: $data) {
           documentId
           item {
+            id
             title
             quantity
             price
@@ -33,27 +36,37 @@ const schema = {
             model
             image
           }
+          user {
+            username
+          }
+          createdAt
+          updatedAt
+          publishedAt
         }
       }
     `),
     updateCart: graphql(`
-      mutation UpdateCart($documentId: ID!, $data: CartInput!) {
+      mutation UpdateCart($documentId: ID!, $data: CartItemInput!) {
         updateCart(documentId: $documentId, data: $data) {
           documentId
           item {
-            image
-            odoo_product_id
-            price
-            quantity
-            model
+            id
             title
+            price
+            model
+            image
+            quantity
+            odoo_product_id
           }
+          createdAt
+          updatedAt
+          publishedAt
         }
       }
     `),
     removeFromCart: graphql(`
-      mutation DeleteCart($documentId: ID!) {
-        deleteCart(documentId: $documentId) {
+      mutation DeleteCartItem($documentId: ID!) {
+        deleteCartItem(documentId: $documentId) {
           documentId
         }
       }
