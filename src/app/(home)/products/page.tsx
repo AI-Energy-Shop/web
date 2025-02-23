2442dc5015adf79193af3ea133a58cf0ff2ad7b4
javdevruent <ruentgen@aienergy.com.au>
2025-02-23 11:15:52 +0800

    remove static filter and made it more dynamic by geting all the specs of product data

--- src/app/(home)/products/page.tsx
+++ src/app/(home)/products/page.tsx
@@ -1,12 +1,12 @@
 export const dynamic = 'force-dynamic';
+
+
 import ProductList from '@/components/products/ProductList';
 import Breadcrumb from '@/components/products/Breadcrumb';
 import Categories from '@/components/products/Categories';
 import Brands from '@/components/products/Brands';
 import { products } from '@/app/actions/products';
 import { PRODUCT_CATEGORIES } from '@/constant';
-import { getProductSpecification } from '@/utils/productArray';
-import { removeDuplicates } from '@/utils/array';
 import PageTitle from '@/components/products/PageTitle';
 
 const INITIAL_PAGE = 1;
@@ -26,52 +26,6 @@ export default async function ProductsPage({
     },
   });
 
-  // const brands = getProductSpecification(productsData, 'Brand');
-  // const powerRatings = getProductSpecification(productsData, 'Power Rating');
-  // const inverterTypes = getProductSpecification(productsData, 'Inverter Type');
-  // const phaseSupport = getProductSpecification(productsData, 'Phase Support');
-  // const gridSupport = getProductSpecification(productsData, 'Grid Support');
-  // const mttps = getProductSpecification(productsData, 'No. of MPPTs');
-  // const prodWarranty = getProductSpecification( productsData, 'Product Warranty');
-
-  // const filters = [
-  //   {
-  //     id: 1,
-  //     name: 'Brands',
-  //     value: removeDuplicates(brands, 'value'),
-  //   },
-  //   {
-  //     id: 2,
-  //     name: 'Power Rating',
-  //     value: removeDuplicates(powerRatings, 'value'),
-  //   },
-  //   {
-  //     id: 3,
-  //     name: 'Inverter Type',
-  //     value: removeDuplicates(inverterTypes, 'value'),
-  //   },
-  //   {
-  //     id: 4,
-  //     name: 'Phase Support',
-  //     value: removeDuplicates(phaseSupport, 'value'),
-  //   },
-  //   {
-  //     id: 4,
-  //     name: 'Grid Support',
-  //     value: removeDuplicates(gridSupport, 'value'),
-  //   },
-  //   {
-  //     id: 5,
-  //     name: 'No. of MPPTs',
-  //     value: removeDuplicates(mttps, 'value'),
-  //   },
-  //   {
-  //     id: 7,
-  //     name: 'Product Warranty',
-  //     value: removeDuplicates(prodWarranty, 'value'),
-  //   },
-  // ];
-
   return (
     <div className="min-h-screen bg-[#fdf6ed]">
       <Breadcrumb />
@@ -85,7 +39,6 @@ export default async function ProductsPage({
           limit={Number(limit) || undefined}
           currentPage={Number(page) || INITIAL_PAGE}
           pageSize={Number(pageSize) || INITIAL_PAGE_SIZE}
-          // filters={filters}
         />
       </div>
     </div>
