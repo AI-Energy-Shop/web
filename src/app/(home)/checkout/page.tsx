a05a2ae76f6278a57c7900d76acf2e0096fe6882
javdevruent <ruentgen@aienergy.com.au>
2025-02-23 11:14:55 +0800

    remove logs

--- src/app/(home)/checkout/page.tsx
+++ src/app/(home)/checkout/page.tsx
@@ -1,15 +1,20 @@
+import { getCartItems } from '@/app/actions/cart';
 import CartDetails from '@/components/checkout/CartDetails';
 import { cookies } from 'next/headers';
 
-function CheckoutPage() {
+async function CheckoutPage() {
   const cookieStore = cookies();
   const a_token = cookieStore.get('a-token');
   const a_user = cookieStore.get('a-user');
+  const { carts } = await getCartItems()
+
+  console.log('CART ITEMS: ', carts);
   return (
     <main className="bg-yellow-light-yellow pb-12">
       <CartDetails
         authToken={a_token?.value}
         userEmail={JSON.parse(a_user?.value || JSON.stringify({})).email}
+        data={carts}
       />
     </main>
   );
