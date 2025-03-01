90536a79805c98373e740403df21e2952fb05b7a
javdevruent <ruentgen@aienergy.com.au>
2025-03-01 11:14:58 +0800

    rename isOpen props to open

--- src/components/Checkout/ModalWrapper.tsx
+++ src/components/Checkout/ModalWrapper.tsx
@@ -10,7 +10,7 @@ import {
 } from '@/components/ui/dialog';
 
 interface ModalProps {
-  isOpen: boolean;
+  open: boolean;
   onClose: () => void;
   onConfirm: () => void;
   title: string;
@@ -19,7 +19,7 @@ interface ModalProps {
 }
 
 const Modal: React.FC<ModalProps> = ({
-  isOpen,
+  open,
   onClose,
   onConfirm,
   title,
@@ -27,7 +27,7 @@ const Modal: React.FC<ModalProps> = ({
   message,
 }) => {
   return (
-    <Dialog open={isOpen}>
+    <Dialog open={open}>
       {/* <DialogTrigger asChild>
         <Button variant="outline">Edit Profile</Button>
       </DialogTrigger> */}
