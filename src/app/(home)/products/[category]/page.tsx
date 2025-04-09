c4352f9f7551bbaff5cf6fb0ace1fc0a8a30134f
javdevruent <ruentgen@aienergy.com.au>
2025-04-09 22:07:06 +0800

    added category condition, remove filter of category value equals to all

--- src/app/(home)/products/[category]/page.tsx
+++ src/app/(home)/products/[category]/page.tsx
@@ -29,7 +29,7 @@ export default async function CategoryPage({
 
   let filters: any = {};
 
-  if (category) {
+  if (category && category !== 'all') {
     filters = {
       categories: {
         slug: {
