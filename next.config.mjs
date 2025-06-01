8c39607ad87b346b7a27ab06c06698e640e1db4c
javdevruent <ruentgen@aienergy.com.au>
2025-06-02 02:37:45 +0800

    init commit dockerizing project

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,32 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  output: 'standalone',
   env: {
     BASE_PROTOCOL: process.env.BASE_PROTOCOL,
     BASE_URL_HOST: process.env.BASE_URL_HOST,
+    DO_SPACES_HOST: process.env.DO_SPACES_HOST,
+    DO_SPACES_PATH: process.env.DO_SPACES_PATH,
+    DO_SPACES_PROTOCOL: process.env.DO_SPACES_PROTOCOL,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    MACSHIP_API_KEY: process.env.MACSHIP_API_KEY,
   },
   images: {
     remotePatterns: [
       {
-        protocol: process.env.DO_SPACES_PROTOCOL,
+        protocol: 'https',
         hostname: process.env.DO_SPACES_HOST,
         pathname: process.env.DO_SPACES_PATH,
         port: '',
       },
     ],
   },
-  transpilePackages: ['lucide-react'], // add this
+  transpilePackages: ['lucide-react'],
   reactStrictMode: true,
   eslint: {
-    dirs: ['pages', 'src/components', 'src/hooks'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+    dirs: ['pages', 'src/components', 'src/hooks'],
   },
   experimental: {
     serverActions: {
